9bb0bd17a4a2cfe6de0fcd6f701bc4abe65cf10c
hymndrazill <srihafiras@yahoo.fr>
2022-11-28 18:51:46 +0100

    deals component and styling

---
+++ walmart/src/components/deals/Deal.tsx
@@ -0,0 +1,13 @@
+import "./deal.css"
+
+const Deal = () => {
+  return (
+    <div className="deals">
+      <h4> Cyber Deals are here! Save on can't-miss finds.</h4>
+      {/* Link here */}
+        <p>Shop Deals</p>
+    </div>
+  )
+}
+
+export default Deal

53e298db2d69d441a8edc9d6f3aa36315bbed811
hymndrazill <srihafiras@yahoo.fr>
2022-11-29 02:08:53 +0100

    adding navbar comp

--- walmart/src/App.tsx
+++ walmart/src/App.tsx
@@ -1,11 +1,12 @@
 import Deal from "./components/deals/Deal"
-
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
 
   return (
     <div className="App">
       <Deal/>
+      <Navbar/>
     </div>
   )
 }

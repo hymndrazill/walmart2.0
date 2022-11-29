037a305ec68b12e2c1107bffa83cb56ab53d4948
hymndrazill <srihafiras@yahoo.fr>
2022-11-29 18:21:12 +0100

    styling and wrapping the nav

--- walmart/src/components/navbar/Navbar.tsx
+++ walmart/src/components/navbar/Navbar.tsx
@@ -9,31 +9,31 @@ const Navbar = () => {
             <div className="image">
                 <img 
                 className="navbar__image" 
-                src="https://1000logos.net/wp-content/uploads/2017/05/Walmart-Logo.png"/>
+                src="//i5.walmartimages.com/dfw/63fd9f59-b3e1/7a569e53-f29a-4c3d-bfaf-6f7a158bfadd/v1/walmartLogo.svg"/>
             </div>
-            <span><BiCategory/>Category</span>
-            <span><BiCategory/>Services</span>
+            <span><BiCategory className="icons"/>Category</span>
+            <span><BiCategory className="icons"/>Services</span>
             <div className="input_search">
                 <input type="text" placeholder="Search Everything At Walmart online and in store" />
-                <AiOutlineSearch/>
+                <AiOutlineSearch size={35}  className="search_icon"/>
             </div>
             <div className="order">
-                <AiFillHeart/>
+                <AiFillHeart className="heart_icon"/>
                 <span >reorder</span>
                 <h3>My Items</h3>
             </div>
             <div className="login">
-                <AiOutlineUser/>
+                <AiOutlineUser className="user_icon"/>
                 <span>Sign in</span>
                 <h3>Account</h3>
             </div>
             <div className="cart">
-                <AiOutlineShoppingCart/>
-                <span>0</span>
-                <h3>$0.00</h3>
+                    <AiOutlineShoppingCart size={30} style={{color:"white"}}className="cart_icon"/>                   
+                     <div className="cart_total">
+                        <div className="total">0</div>
+                     </div>
+                    <span >$0.00</span>
             </div>
-
-
         </div>
 
 

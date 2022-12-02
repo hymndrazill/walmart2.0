3dce1d2e87861b759c288711ea41af6059728f05
hymndrazill <srihafiras@yahoo.fr>
2022-12-02 22:48:53 +0100

    adding section and currently styling

---
+++ walmart/src/components/sideSection/Section.tsx
@@ -0,0 +1,11 @@
+import "./section.css"
+
+const Section = () => {
+  return (
+    <div className="section_container">
+        <img className="section_img"src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+    </div>
+  )
+}
+
+export default Section

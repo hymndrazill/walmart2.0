4d49186cd8b6b68131fd4dd9a1c8d42f25bb9c4e
hymndrazill <srihafiras@yahoo.fr>
2022-12-02 22:48:13 +0100

    adding container for the slider and section-right

--- walmart/src/App.tsx
+++ walmart/src/App.tsx
@@ -1,14 +1,15 @@
+import Container from "./components/container/Container"
 import Deal from "./components/deals/Deal"
 import Header from "./components/header/Header"
 import Navbar from "./components/navbar/Navbar"
-
+import "./App.css"
 function App() {
-
   return (
     <div className="App">
       <Deal/>
       <Navbar/>
       <Header/>
+      <Container/>
     </div>
   )
 }

540808de72566639fef8a0a6cbc10cdafb378683
hymndrazill <srihafiras@yahoo.fr>
2022-12-02 22:49:09 +0100

    adding a container for the slider and section

---
+++ walmart/src/components/container/Container.tsx
@@ -0,0 +1,14 @@
+import Section from "../sideSection/Section"
+import Slider from "../slider/Slider"
+import "./container.css"
+
+const Container = () => {
+  return (
+    <div className="container">
+        <Slider/>
+        <Section/>
+    </div>
+  )
+}
+
+export default Container

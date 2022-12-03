828c0a210fe1ce359bdba578ef84951070c91f8b
hymndrazill <srihafiras@yahoo.fr>
2022-12-03 02:05:24 +0100

    just changed fonts and colors

--- walmart/src/components/sideSection/Section.tsx
+++ walmart/src/components/sideSection/Section.tsx
@@ -3,7 +3,8 @@ import "./section.css"
 const Section = () => {
   return (
     <div className="section_container">
-        <img className="section_img"src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <img className="section_img"
+        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
         <div className="section_details">
           <p>Find gifts, holiday faves & more.</p>
           <h3>Major Savings</h3>

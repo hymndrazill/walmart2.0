f0f239e0acf1d493f748ca0b272c09a40eaeadfb
hymndrazill <srihafiras@yahoo.fr>
2022-12-03 02:00:07 +0100

    finishing the section

--- walmart/src/components/sideSection/Section.tsx
+++ walmart/src/components/sideSection/Section.tsx
@@ -4,6 +4,11 @@ const Section = () => {
   return (
     <div className="section_container">
         <img className="section_img"src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="section_details">
+          <p>Find gifts, holiday faves & more.</p>
+          <h3>Major Savings</h3>
+          <span>Shop Now</span>
+        </div>
     </div>
   )
 }

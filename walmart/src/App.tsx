4dbf9394779f3d56d1009206fdb721363d5989fc
hymndrazill <srihafiras@yahoo.fr>
2022-12-19 00:16:41 +0100

    starting the cart page + deal props

--- walmart/src/App.tsx
+++ walmart/src/App.tsx
@@ -2,12 +2,17 @@ import Container from "./components/container/Container"
 import Deal from "./components/deals/Deal"
 import Header from "./components/header/Header"
 import Navbar from "./components/navbar/Navbar"
+import Cart, { deal } from "./pages/cart/Cart"
 // import "./App.css"
 import Login from "./pages/login/Login"
-function App() {
+function App({deal}:deal) {
   return (
     <div className="App">
-      <Login/>
+      <Cart  deal={deal}/>
+     
+      
+
+      {/* <Login/> */}
       {/* <Deal/>
       <Navbar/>
       <Header/>

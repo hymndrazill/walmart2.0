778a661dd9851584101e24e5bb4f43bffbaeb6cf
hymndrazill <srihafiras@yahoo.fr>
2022-12-19 02:27:03 +0100

    prop for Cart component

--- walmart/src/App.tsx
+++ walmart/src/App.tsx
@@ -2,13 +2,13 @@ import Container from "./components/container/Container"
 import Deal from "./components/deals/Deal"
 import Header from "./components/header/Header"
 import Navbar from "./components/navbar/Navbar"
-import Cart, { deal } from "./pages/cart/Cart"
-// import "./App.css"
+import Cart from "./pages/cart/Cart"
+import "./App.css"
 import Login from "./pages/login/Login"
-function App({deal}:deal) {
+function App() {
   return (
     <div className="App">
-      <Cart  deal={deal}/>
+      <Cart  />
      
       
 
